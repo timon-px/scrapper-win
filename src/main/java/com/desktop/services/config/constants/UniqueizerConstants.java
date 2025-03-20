41da67d360d67e2413c2cc08156b69a425795281
timon-px <team@px-traff.company>
2025-03-20 16:43:38 +0200

    changed file saving

--- src/main/java/com/desktop/services/config/constants/UniqueizerConstants.java
+++ src/main/java/com/desktop/services/config/constants/UniqueizerConstants.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 public class UniqueizerConstants {
-    public static final String UNIQUE_HTML_NAME = "index_unique.html";
+    public static final String UNIQUE_SUFFIX_NAME = "_unique";
     public static final int RANDOM_STRING_LENGTH = 10;
     public static final int MIN_EMPTY_DIVS = 1;
     public static final int MAX_EMPTY_DIVS = 5;
